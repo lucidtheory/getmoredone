45c1d882e01c1a48123e5ed74d8e50d90e28d814
Esdras Etrenne <esdras.pierre91@gmail.com>
2021-03-17 06:36:16 -0500

    remove unnecessary data

--- src/getItems.js
+++ src/getItems.js
@@ -4,13 +4,8 @@ const { log } = Apify.utils;
 
 async function getItems(pageObj, pageData, resultsArr) {
     // Scrape all items that match the selector
-    const itemsObj = await pageObj.$$eval('div.p13n-sc-truncated', prods => prods.map(prod => prod.innerHTML));
-
-    const pricesObj = await pageObj.$$eval('span.p13n-sc-price', price => price.map(el => el.innerHTML));
 
     const urlsObj = await pageObj.$$eval('span.aok-inline-block > a.a-link-normal', link => link.map(url => url.href));
-
-    const imgsObj = await pageObj.$$eval('a.a-link-normal > span > div.a-section > img', link => link.map(url => url.src));
 
     // Get rid of duplicate URLs (couldn't avoid scraping them)
     const urlsArr = [];
@@ -25,10 +20,7 @@ async function getItems(pageObj, pageData, resultsArr) {
         resultsArr.push({
             ...pageData,
             ID: resultsArr.length,
-            name: itemsObj[i],
-            price: pricesObj[i],
             url: urlsArr[i],
-            thumbnail: imgsObj[i],
             asin: urlsArr[i]?.split("/dp/")[1]?.split("/")[0]
         });
     }
