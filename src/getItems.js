2b38cd21a258f6a2d04c531de304ae67fe255735
Esdras Etrenne <esdras.pierre91@gmail.com>
2021-03-17 05:18:31 -0500

    fix data type

--- src/getItems.js
+++ src/getItems.js
@@ -5,7 +5,7 @@ const { log } = Apify.utils;
 async function scrapeDetailsPage(pageObj) {
     const resultsArr = [];
     // get page 1 url
-    const page1Url = await pageObj.url();
+    const page1Url = {url: await pageObj.url()};
     log.info(`this is the page1url: ${page1Url}`);
     resultsArr.push(page1Url);
 
@@ -23,7 +23,7 @@ async function scrapeDetailsPage(pageObj) {
         await pageObj.waitForNavigation();
         const page2Url = await pageObj.url();
         log.info('we got page 2 and are pushing')
-        resultsArr.push(page2Url);
+        resultsArr.push({url: page2Url});
         log.info('we are pushing to apify')
         await Apify.pushData(resultsArr);
         log.info(`Saving results from ${page2Url}`);
